c014aea58e6eff6c27c221bc5582b40f80d4cd64
Breno Castro <breno.castro.ofc@gmail.com>
2025-06-06 21:20:21 -0300

    adjusted fix property initialization for BuildVendorError

--- src/domain/vendor/errors/vendor.errors.ts
+++ src/domain/vendor/errors/vendor.errors.ts
@@ -1,7 +1,7 @@
 import { AppError } from '@/core/errors/app-error';
 
 export class BuildVendorError extends AppError {
-  readonly code: 'vendor-entity.build-vendor';
+  readonly code = 'vendor-entity.build-vendor';
 
   constructor() {
     super('Tivemos um problema na estruturação dos dados');
