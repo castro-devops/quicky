bbc028ac9ba75d306849dba3e8c864db47d9abc1
RASCUNHO papel e design <breno.castro.ofc@gmail.com>
2025-06-10 22:51:51 -0300

    inited controller register vendor

--- src/infra/http/vendors/controllers/register-vendor.controller.ts
+++ src/infra/http/vendors/controllers/register-vendor.controller.ts
@@ -1,7 +1,31 @@
 import { RegisterVendorUseCase } from '@/app/use-cases/vendor/register-vendor.use-case';
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RegisterVendorDto } from '../schemas/register-vendor.dto';
+import dayjs from '@/core/config/dayjs.config';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../../pipes/zod-validation.pipe';
+
+const bodySchemaRequest = z.object({
+  name: z.string(),
+  surname: z.string(),
+  birth: z.date().optional(),
+  email: z.string().email(),
+  phone: z.string().max(11),
+  companyName: z.string().optional(),
+  document: z.string().optional(),
+  plan: z.enum(['free', 'pro', 'enterprise']).default('free'),
+  planExpiresAt: z.date().default(dayjs().add(7, 'day').toDate()),
+});
+
+type TBodySchemaRequest = z.infer<typeof bodySchemaRequest>;
 
 @Controller({
   path: 'api/v1/vendor',
@@ -14,5 +38,8 @@ export class RegisterVendorController {
   @ApiOperation({ summary: 'Cria uma nova conta' })
   @ApiBody({ type: RegisterVendorDto })
   @HttpCode(HttpStatus.CREATED)
-  register() {}
+  @UsePipes(new ZodValidationPipe(bodySchemaRequest))
+  register(@Body() body: TBodySchemaRequest) {
+    console.log(body);
+  }
 }
